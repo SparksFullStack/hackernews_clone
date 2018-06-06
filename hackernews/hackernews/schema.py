73473cd22a46de6475f8ad76f80ee4146f75feed
Charlie Sparks <csparks@antioch.edu>
2018-06-06 13:37:45 -0700

    added a create user mutation

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -1,13 +1,14 @@
 import graphene
 
 import links.schema
+import users.schema
 
 # this Query automatically inherits the Query defined in links.schema
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 # this Mutation works just like the Query in that it automatically inherits the Mutation from links.schema
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType,):
     pass
 
 # this sets the global Schema values for the different types: Queries, Mutations, and Subscriptions
